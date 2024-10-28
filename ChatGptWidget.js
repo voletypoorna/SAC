f680181c31a28c1c177659ddc1443f58b8cfa221
voletypoorna <161069497+voletypoorna@users.noreply.github.com>
2024-10-28 19:10:31 -0400

    Update ChatGptWidget.js

--- ChatGptWidget.js
+++ ChatGptWidget.js
@@ -136,5 +136,5 @@
       this.initMain();
     }
   }
-  customElements.define("com-poornavolety-jnj-ChatGptwidget", Widget);
+  customElements.define("com-poornavolety-jnj-chatgptwidget", Widget);
 })();
